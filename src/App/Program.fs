856fbca5b9d8f9876a8a7318d5a958b5928a5973
wasuken <wevorence@gmail.com>
2020-01-08 23:55:15 +0900

    first commit.

---
+++ src/App/Program.fs
@@ -0,0 +1,14 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open Mail.Imap
+open System.Collections.Generic
+open System.Net.Mail
+open System.Linq
+
+[<EntryPoint>]
+let main argv =
+  let msgs:IEnumerable<MailMessage> = getMessages "hoge" "hoge" "hoge" 993
+  let formattedMsgList = msgs.Select(fun msg -> (printfn "%s <%s>\nbody:\n%s" msg.Subject msg.From.Address msg.Body))
+  Console.WriteLine("{0}", System.String.Join("\n", formattedMsgList))
+  0
---
+++ src/Mail/Library.fs
@@ -0,0 +1,12 @@
+﻿namespace Mail
+
+open S22.Imap
+open System.Collections.Generic
+open System.Net.Mail
+open System.Linq
+
+module Imap =
+  let getMessages (username: string) (password: string) (server: string) (port: int) : IEnumerable<MailMessage> =
+    let client = new ImapClient(server, port, username, password, AuthMethod.Login, true)
+    let uids : IEnumerable<uint32> = client.Search(SearchCondition.All())
+    uids.Select(fun x -> client.GetMessage(x, FetchOptions.HeadersOnly))
