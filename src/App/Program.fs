956796f7508fd3d4ee2d4a4c7fc8aab312703734
wasuken <wevorence@gmail.com>
2020-02-21 22:18:56 +0900

    change lib fsharp-dapper.

--- src/App/Program.fs
+++ src/App/Program.fs
@@ -3,33 +3,20 @@
 open System
 open IOMail.Imap
 open IOConfig.Ini
+open IOConfig.Queries.Mail
+open IOConfig.Connection
 open System.Collections.Generic
 open System.Net.Mail
 open System.Linq
-open FSharp.Data.Sql
-
-[<Literal>]
-let connectionString =
-    "Data Source=" +
-    __SOURCE_DIRECTORY__ + @"/db.sqlite;" +
-    "Version=3;foreign keys=true"
-
-type Database = SqlDataProvider<
-  Common.DatabaseProviderTypes.SQLITE,
-  SQLiteLibrary = Common.SQLiteLibrary.SystemDataSQLite,
-  ConnectionString = connectionString>
-
-let context = Database.GetDataContext()
 
 [<EntryPoint>]
 let main argv =
   let iniMap = readIniToDict "config.ini"
   let msgs: IEnumerable<MailMessage> =
     getMessages iniMap.["UNAME"] iniMap.["PASS"] iniMap.["SNAME"]  (iniMap.["PORT"] |> int)
-  for msg in msgs do
-    printfn "title<from>:%s <%s>\n" msg.Subject msg.From.Address
-    let mail = context.Main.Mail.Create()
-    mail.Subject <- msg.Subject
-    mail.FromAddr <- msg.From.Address
-    context.SubmitUpdates()
+  for msg in msgs.Take(10) do
+    insertMail msg.Subject msg.From.Address |> Async.RunSynchronously
+  let rows = selectMail |> Async.RunSynchronously
+  for row in rows do
+    printfn "title<from>:%s <%s>\n" row.Subject row.FromAddr
   0
--- src/IOConfig/Library.fs
+++ src/IOConfig/Library.fs
@@ -4,6 +4,8 @@ open System
 open System.IO
 open System.Linq
 open System.Collections.Generic
+open FSharp.Data.Dapper
+open Microsoft.Data.Sqlite
 
 module Ini =
   let readIniToDict (path: string) =
@@ -12,3 +14,35 @@ module Ini =
     for x in lineSps do
       dict.Add(x.First(), String.Join("", x.Skip(1)))
     dict
+
+module Connection =
+    let private mkConnectionString (dataSource : string) =
+        sprintf
+            "Data Source = %s;"
+            dataSource
+
+    let mkShared () = new SqliteConnection (mkConnectionString (__SOURCE_DIRECTORY__ + "/prod.sqlite"))
+
+module Types =
+    [<CLIMutable>]
+    type Mail =
+        { Id         : int64
+          Subject       : string
+          FromAddr       : string
+          }
+
+module Queries =
+    let private connectionF () = Connection.SqliteConnection (Connection.mkShared())
+
+    let querySeqAsync<'R>          = querySeqAsync<'R> (connectionF)
+    let querySingleAsync<'R>       = querySingleAsync<'R> (connectionF)
+    let querySingleOptionAsync<'R> = querySingleOptionAsync<'R> (connectionF)
+
+    module Mail =
+      let insertMail subject fromAddr = querySingleOptionAsync<Types.Mail>{
+        script "insert into mail(subject, from_addr) values(@Subject, @FromAddr);"
+        parameters (dict [ "Subject", box subject; "FromAddr", box fromAddr ])
+      }
+      let selectMail = querySeqAsync<Types.Mail> {
+        script "select * from mail;"
+      }
